1bd01a272bbd54082c478e98f259eca68661cd43
carlosmurilodn <carlos05111991@hotmail.com>
2025-04-17 15:05:53 -0300

    Gerando as views do devise 2

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   root to: "home#index"
+
+  devise_scope :user do
+   get "/users/sign_out" => "devise/sessions#destroy"
+end
 end
